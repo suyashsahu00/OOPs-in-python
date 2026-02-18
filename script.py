bef2b75e4f077a5f350189d4a7ee9e1d0baac0a2
suyashsahu00 <suyashsahu00@gmail.com>
2026-02-18 19:38:17 +0530

    feat: Implement Owner class and associate dogs with owner objects.

--- script.py
+++ script.py
@@ -1,20 +1,35 @@
 class Dog:
 
-    def __init__(self, name, breed):
+    def __init__(self, name, breed, owner):
         self.name = name  # Data Field
         self.breed = breed
+        self.owner = owner
 
     def bark(self):
         print("Whoof Whoof")
 
 
-# dog object and assigning it to dog variable
-dog1 = Dog("Brue", "Scottish Terrier")
-dog1.bark()  # accesing method on object
-print(dog1.name)
-print(dog1.breed)
+class Owner:
+    def __init__(self, name, address, contact_number):
+        self.name = name
+        self.address = address
+        self.phone_number = (
+            contact_number  # data field = phone no. parameter field = contact_number
+        )
 
-dog2 = Dog("Freya", "Greyhound")
+
+owner1 = Owner("Suyash", "India", "666-999")
+
+# dog object and assigning it to dog variable
+dog1 = Dog("Bruce", "Scottish Terrier", owner1)
+print(dog1.owner.name)
+# dog1.bark()  # accesing method on object
+# print(dog1.name)
+# print(dog1.breed)
+owner2 = Owner("Suyash2", "India", "666-999")
+
+dog2 = Dog("Freya", "Greyhound", owner2)
 dog2.bark()
 print(dog2.name)
 print(dog2.breed)
+print(dog2.owner.name)
