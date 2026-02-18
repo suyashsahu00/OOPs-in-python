8e016eb68037a5376091186cc844614a3e433b9d
suyashsahu00 <suyashsahu00@gmail.com>
2026-02-18 19:22:10 +0530

    feat: Implement a `Dog` class with attributes, methods, and object instantiation.

--- script.py
+++ script.py
@@ -1,6 +1,20 @@
-name = "Danny"
-age = 29
+class Dog:
 
-print(type(name.upper()))
-print(name.upper())
-print(type(age))
+    def __init__(self, name, breed):
+        self.name = name  # Data Field
+        self.breed = breed
+
+    def bark(self):
+        print("Whoof Whoof")
+
+
+# dog object and assigning it to dog variable
+dog1 = Dog("Brue", "Scottish Terrier")
+dog1.bark()  # accesing method on object
+print(dog1.name)
+print(dog1.breed)
+
+dog2 = Dog("Freya", "Greyhound")
+dog2.bark()
+print(dog2.name)
+print(dog2.breed)
