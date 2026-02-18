bbea804a8af5964063d7802097ccff008fe8dc56
suyashsahu00 <suyashsahu00@gmail.com>
2026-02-18 20:36:52 +0530

    refactor: Replaced `Dog` and `Owner` class examples with a `Person` class example.

--- script.py
+++ script.py
@@ -1,35 +1,14 @@
-class Dog:
-
-    def __init__(self, name, breed, owner):
-        self.name = name  # Data Field
-        self.breed = breed
-        self.owner = owner
-
-    def bark(self):
-        print("Whoof Whoof")
-
-
-class Owner:
-    def __init__(self, name, address, contact_number):
+class Person:
+    def __init__(self,name,age):
         self.name = name
-        self.address = address
-        self.phone_number = (
-            contact_number  # data field = phone no. parameter field = contact_number
-        )
+        self.age = age
+
+    def greet(self):
+        print(f"Hello my name is {self.name} and my age is {self.age}")
 
 
-owner1 = Owner("Suyash", "India", "666-999")
+person1 = Person("Suyash",30)
+person1.greet()
 
-# dog object and assigning it to dog variable
-dog1 = Dog("Bruce", "Scottish Terrier", owner1)
-print(dog1.owner.name)
-# dog1.bark()  # accesing method on object
-# print(dog1.name)
-# print(dog1.breed)
-owner2 = Owner("Suyash2", "India", "666-999")
-
-dog2 = Dog("Freya", "Greyhound", owner2)
-dog2.bark()
-print(dog2.name)
-print(dog2.breed)
-print(dog2.owner.name)
+person2 = Person("Suyash2",30)
+person2.greet()
