757b1cf79ef577dbae251f3709dd8fb24abe1aa1
suyashsahu00 <suyashsahu00@gmail.com>
2026-02-22 20:41:52 +0530

    feat: Introduce Animal, Dog, and GuideDog classes to demonstrate multi-level inheritance.

--- TASK/Inherit1.py
+++ TASK/Inherit1.py
@@ -9,3 +9,14 @@ class Dog(Animal):
 d = Dog("Tommy")
 d.eat() # inherited from Animal
 d.bark() # Dog's own method!
+
+class GuideDog(Dog):
+    def __init__(self,name,owner):
+        super().__init__(name) # pass name up to Animal
+        self.owner = owner # extra attribute
+    def guide(self):
+        print(f"{self.name} is guiding {self.owner}")
+g = GuideDog("Tommy", "Suyash")
+g.eat() # from Animal ^^
+g.bark() # from Dog ^
+g.guide() # own method 
