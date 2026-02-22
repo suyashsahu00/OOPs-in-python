23351567d652877e241d077dc73a9b8c685b4237
suyashsahu00 <suyashsahu00@gmail.com>
2026-02-22 19:45:01 +0530

    feat: Add `Person` class with `name` and `age` attributes, `introduce` and `birthday` methods, and usage examples.

--- TASK/Class1.py
+++ TASK/Class1.py
@@ -5,9 +5,16 @@ class Person:
 
     def introduce(self):
         print(f"Hi I am {self.name} and I am {self.age} years old.")
+    def birthday(self):
+        self.age = self.age +1
+        print(f"Happy Birthday {self.name}! You are now {self.age} year old")
+
 
 
 p1 = Person("Suyash", 21)
 p2 = Person("Rahul", 22)
 p1.introduce()  # prints automatically!
 p2.introduce()  # prints automatically!
+print(p1.age) # before
+p1.birthday() # Call birthday
+print(p1.age) # after - should be +1
