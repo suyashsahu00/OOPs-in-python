fd89249f073c863555e11ae22dffd03bafcf35e4
suyashsahu00 <suyashsahu00@gmail.com>
2026-02-22 18:11:50 +0530

    feat: Add Calculator and Dog classes with basic functionalities.

---
+++ TASK/Calculator.py
@@ -0,0 +1,8 @@
+class Calculator:
+    def add(self,a,b):
+        return a+b
+    def multiply(self, a,b):
+        return a * b
+calc = Calculator()# no arugment needed!
+print(calc.add(1,2))
+print(calc.multiply(1,2))
